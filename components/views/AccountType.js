2362f7a0493cd157770b1ea9f45f44fba192823b
ezmapel <ezmapel@EthanMapelsMBP.attlocal.net>
2020-04-12 14:11:38 -0500

    Add acctType banner and updated patientPortal photo

--- components/views/AccountType.js
+++ components/views/AccountType.js
@@ -4,14 +4,12 @@ export default (st) => `
 <button id="userLoggedIn"></button>
 </div>
 
-<h2 class="showMe">${st.Header}</h2>
 <section id="jumbotronAccountType">
-<div id="vertcenter" class="center">
-<img class="imageOne" src="">
-</div>
-</section>
-
+<section class="jumbotronAccountText">
+<h1 class="headerAcct">${st.Header}</h1>
 <div class="center">
 <button class="acctTypeButton createUserDoc">I am a new DOCTOR. Create my account now!</button>
 <button class="acctTypeButton createUserPatient">I am a new PATIENT. Create my account now!</button>
-</div>`;
+</div>
+</section>
+</section>`;
--- components/views/PatientPortal.js
+++ components/views/PatientPortal.js
@@ -1,9 +1,29 @@
-export default st => `
+export default (st) => `
 <div class="underNav">
 <button class="hiddenButton"></button>
 <button id="userLoggedIn"></button>
 </div>
 
-<h2 class="showMe">${st.Header}</h2>
+<section id="jumbotronPatientPortal">
+<section class="portalText">
+<div>
+<h2 class="headerAcct">${st.Header}</h2>
+</div>
+<div class="splitPortal">
 <button class="portalButton searchResults"=>Search Doctors</button>
-<button class="portalButton patientBookings"=>View Your Upcoming Appointments</button>`;
+<button class="portalButton patientBookings"=>View Your Upcoming Appointments</button>
+</div>
+</section>
+</section>`;
+
+{
+  /* <section id="jumbotronAccountType">
+<section class="jumbotronAccountText">
+<h1 class="headerAcct">${st.Header}</h1>
+<div class="center">
+<button class="acctTypeButton createUserDoc">I am a new DOCTOR. Create my account now!</button>
+<button class="acctTypeButton createUserPatient">I am a new PATIENT. Create my account now!</button>
+</div>
+</section>
+</section> */
+}
